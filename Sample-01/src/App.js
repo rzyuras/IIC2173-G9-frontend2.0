5a05981ca8449396abfc5fdf7500207b774b685f
Bárbara Cabezas <barbara@Barbaras-MacBook-Air.local>
2024-04-21 14:38:08 -0400

    avance

--- Sample-01/src/App.js
+++ Sample-01/src/App.js
@@ -3,10 +3,8 @@ import { Router, Route, Switch } from "react-router-dom";
 import { Container } from "reactstrap";
 import Loading from "./components/Loading";
 import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
 import Home from "./views/Home";
 import Profile from "./views/Profile";
-import ExternalApi from "./views/ExternalApi";
 import { useAuth0 } from "@auth0/auth0-react";
 import history from "./utils/history";
 
@@ -40,7 +38,6 @@ const App = () => {
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/profile" component={Profile} />
-            <Route path="/external-api" component={ExternalApi} />
             <Route path="/flights" component={FlightList}/>
             <Route path="/details/:flightId" component={Flight}/>
             <Route path="/mypurchases" component={PurchaseList} />
