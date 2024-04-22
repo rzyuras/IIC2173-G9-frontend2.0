4efc29699c61e9a8bc62fb36022bf149595134be
Radoslav Yuras <rzyuras@uc.cl>
2024-04-22 04:02:33 -0400

    feat(fix): Creo que se soluciona que se cae al refrescar

--- Sample-01/src/App.js
+++ Sample-01/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { Container } from "reactstrap";
-import Loading from "./components/Loading";
+// import Loading from "./components/Loading";
 import NavBar from "./components/NavBar";
 import Home from "./views/Home";
 import Profile from "./views/Profile";
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 import history from "./utils/history";
 
 // styles
@@ -20,15 +20,15 @@ import PurchaseList from "./views/MyPurchases";
 initFontAwesome();
 
 const App = () => {
-  const { isLoading, error } = useAuth0();
+  // const { isLoading, error } = useAuth0();
 
-  if (error) {
-    return <div>Oops... {error.message}</div>;
-  }
+  // if (error) {
+  //   return <div>Oops... {error.message}</div>;
+  // }
 
-  if (isLoading) {
-    return <Loading />;
-  }
+  // if (isLoading) {
+  //   return <Loading />;
+  // }
 
   return (
     <Router history={history}>
