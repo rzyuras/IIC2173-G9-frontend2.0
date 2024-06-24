ecc70af1276921d8558b2ee0c9367b5e9e4d66cd
Bárbara Cabezas <barbara@Barbaras-Air.bbrouter>
2024-06-24 03:42:51 -0400

    fix

--- Sample-01/src/components/PopUp.js
+++ Sample-01/src/components/PopUp.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Popup = ({ onClose, children }) => {
+const Popup = ({ onClose, children, style='' }) => {
   return (
     <div className='back-modal'>
-    <div className="modal" onClick={(e) => e.stopPropagation()}>
+    <div className={`modal ${style}`} onClick={(e) => e.stopPropagation()}>
       <div className="modal-content">
         <span className="close" onClick={onClose}>&times;</span>
         {children}
